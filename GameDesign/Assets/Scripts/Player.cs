689a89268b9ed85b138a6fa35e1c417809d82c29
Alexandre_FV_Fonseca <alexandrefvfonseca.37@gmail.com>
2025-11-24 18:45:31 +0000

    Fixed rotation for player and allowed for distance interaction

--- GameDesign/Assets/Scripts/Player.cs
+++ GameDesign/Assets/Scripts/Player.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     public int speed;
+    public GameObject memoryWarning;
+    public KeyCode interactKey;
 
     private float movementX, movementY;
+    private string memoryGateTag = "MemoryGate";
+    private string memoryLocation = "";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,6 +21,7 @@ public class Player : MonoBehaviour
     void Update()
     {
         PlayerMovement();
+        CheckSurrondings();
     }
 
     void PlayerMovement()
@@ -25,5 +31,33 @@ public class Player : MonoBehaviour
 
         //Debug.Log("move x value is: " + movementX);
         transform.position += new Vector3(movementX, movementY, 0f) * Time.deltaTime * speed;
+    }
+
+    void CheckSurrondings()
+    {
+        // Check if there is contact
+        if (Input.GetKeyDown(interactKey) && memoryLocation != "")
+        {
+            SceneManager.LoadScene(memoryLocation);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag(memoryGateTag))
+        {
+            Debug.Log("Touching");
+            memoryWarning.SetActive(true);
+            memoryLocation = collision.gameObject.GetComponent<ChangeScene>().sceneName;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag(memoryGateTag))
+        {
+            memoryWarning.SetActive(false);
+            memoryLocation = "";
+        }
     }
 }
