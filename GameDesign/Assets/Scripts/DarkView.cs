c427e6207f912359d4276db40d77717e2eb7ff22
Henrique Ponte <107682582+henriquePonte@users.noreply.github.com>
2025-11-24 16:17:38 +0000

    dark view effect

---
+++ GameDesign/Assets/Scripts/DarkView.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class DarkView : MonoBehaviour
+{
+    public Transform player; 
+
+    void Update()
+    {
+        transform.position = player.position; // segue o jogador
+    }
+}
