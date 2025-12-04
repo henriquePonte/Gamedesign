5a26bc9f32232c821087c51845fd462e31b38b30
MateusQueiroz03 <130449093+MateusQueiroz03@users.noreply.github.com>
2025-12-04 15:39:59 +0000

    Criado o MainMenu e o PauseMenu

---
+++ GameDesign/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour 
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Casa"); // nome da primeira cena
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
