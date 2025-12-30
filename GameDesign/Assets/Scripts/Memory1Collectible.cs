fd762d81e8d146932491303a43ed2a9025719b76
MateusQueiroz03 <130449093+MateusQueiroz03@users.noreply.github.com>
2025-12-30 16:54:41 +0000

    Lógica de coletar implementada na memória 1

--- GameDesign/Assets/Scripts/Memory1Collectible.cs
+++ GameDesign/Assets/Scripts/Memory1Collectible.cs
@@ -1,14 +1,13 @@
 using UnityEngine;
 
 public class Memory1Collectible : MonoBehaviour
-{
-    [SerializeField] private int pieceID;
-
+{  
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (!other.CompareTag("Player")) return;
-
-        Memory1Manager.Instance.CollectPiece(pieceID);
-        Destroy(gameObject);
+        if (other.CompareTag("Player"))
+        {        
+            Memory1Manager.Instance.CollectPiece();
+            Destroy(gameObject);
+        }
     }
 }
--- GameDesign/Assets/Scripts/Memory1Manager.cs
+++ GameDesign/Assets/Scripts/Memory1Manager.cs
@@ -5,26 +5,22 @@ public class Memory1Manager : MonoBehaviour
 {
     public static Memory1Manager Instance;
 
-    [SerializeField] private int totalPieces; //to be defined? both floors
-    private HashSet<int> collectedPieces = new HashSet<int>();
+    public int totalPieces;
+    public int collectedPieces = 0;
 
     private void Awake()
     {
-        if (Instance == null) Instance = this;
-        else Destroy(gameObject);
+        Instance = this;
     }
 
-    public void CollectPiece(int pieceID)
+    public void CollectPiece()
     {
-        if (collectedPieces.Contains(pieceID)) return;
+        collectedPieces++;
+        Debug.Log($"Piece collected: {collectedPieces}/{totalPieces}");
 
-        collectedPieces.Add(pieceID);
-
-        Debug.Log($"Piece collected: {collectedPieces.Count}/{totalPieces}");
-
-        if (collectedPieces.Count >= totalPieces)
+        if (collectedPieces >= totalPieces)
         {
-            MemoryCompleted();
+           MemoryCompleted();
         }
     }
 
